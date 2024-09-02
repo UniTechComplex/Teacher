dd26b25d6d90e553e8f4f2a11cbe05a6c46010be
Unitech Complex <unitech.complex@gmail.com>
2024-09-02 20:30:20 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+//step 1: get DOM
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function(){
+    showSlider('next');    
+}
+
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
+
+
+
+/* Send the action to the request page*/
+let about = document.querySelector('#about') ;
+about.addEventListener('click',()=>{
+  open('about');
+})
+
+let contact = document.querySelector('#contact') ;
+contact.addEventListener('click',()=>{
+  open('contact');
+})
+
+
+
+function open (a){
+    // Set the countdown time in seconds
+let countdownTime = 1;
+   console.log(a)
+
+
+// Update the countdown every second
+const countdownInterval = setInterval(() => {
+    countdownTime--;
+
+    // When the countdown reaches zero, open the file and clear the interval
+    if (countdownTime < 0) {
+        clearInterval(countdownInterval);
+        window.location.href = '/Experiment/E2/'+a+'.html'; // Replace with the path to your file
+    }
+}, 1000);
+
+
+};
